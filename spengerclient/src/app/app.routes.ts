21864100bfa14e863f24f7bd796b24b85e2f5e9f
oez22163 <oez22163@spengergasse.at>
2024-05-31 12:38:29 +0200

    99% done

--- spengerclient/src/app/app.routes.ts
+++ spengerclient/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { UsersComponent } from './users/users.component';
-import { UserdetailsComponent } from './userdetails/userdetails.component';
-import { PostsComponent } from './posts/posts.component';
+//import { UsersComponent } from './users/users.component';
+//import { UserdetailsComponent } from './userdetails/userdetails.component';
+//import { PostsComponent } from './posts/posts.component';
 import { PatientsComponent } from './patients/patients.component';
 
 export const routes: Routes = [
